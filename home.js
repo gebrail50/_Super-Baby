5d9a5ed6bd6e79bf6052ff597322c778f69dbc33
gebrail50 <57536830+gebrail50@users.noreply.github.com>
2021-06-16 10:29:00 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+var mycover=document.getElementById("header")
+
+/*var images=['../images/bgc1.png','../images/cov22.png']
+ //mycover.style.background='url("../images/cov22.png") no-repeat red'
+ var i=0;
+ var slidershow=function(){
+     mycover.style.background=`url(${images[i]}) no-repeat red`;
+     if(i<images.length-1){
+         i++;
+     }else{
+         i=0;
+     }
+     setTimeout("slidershow()",3000);
+ }
+ slidershow();*/
